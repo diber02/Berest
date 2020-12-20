a5acc4d0746bd3aa36f15b834d7b06f3ff7d7694
diana <diana>
2020-12-20 23:47:20 +0200

    bubble

--- lab06/1/main.c
+++ lab06/1/main.c
@@ -1,8 +1,40 @@
 
-#include <stdio.h>
-
-int main () {
  
-int arr[] =  
-  return 0;
- }
+ #include <stdio.h>
+#define N 5
+
+int main()
+{
+    int arr[N] = {1, 3, 8, 9, 5};
+    int temp = 0;
+    char symbol = 'u';
+    
+    switch (symbol) {
+        case 'u' :
+          for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N-i-1; j++){
+                if ( arr[j] > arr[j+1]) {
+                    temp = arr [j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+       break;
+        
+        
+        case 'd' :
+           for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N-i-1; j++){
+                if ( arr[j] < arr[j+1]) {
+                    temp = arr [j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+       break;
+    } 
+    
+    return 0;
+}
